0cf5025b0d16ecd4cbc8357a2423e321899944d9
BHARTESH KAUSHIK <60025263+dirtylegs@users.noreply.github.com>
2021-12-01 22:02:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const { nextTick } = require('process');
+const port = 8000;
+
+//const db = require('./config/mongoose');
+//const Contact = require('./models/contact');
+
+const router = express();
+
+router.set('view engine', 'ejs');
+router.set('views', path.join(__dirname,'views'));
+router.use(express.urlencoded()); //middleware
+router.use(express.static('assets')); //static
+
+
+router.get('/', function(req, res) {
+    res.render('home');
+})
+
+router.listen(port, function(err) {
+
+    if(err) {
+        return;
+    }
+
+    console.log('server is up and running on port', port);
+})
