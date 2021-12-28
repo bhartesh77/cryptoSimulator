ab9b0b9bd5e4a0433f97528c2a83b3f14ea5e7b0
BHARTESH KAUSHIK <60025263+dirtylegs@users.noreply.github.com>
2021-12-28 22:43:48 +0530

    post router

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 const path = require('path');
 const { nextTick } = require('process');
 const port = 8000;
@@ -14,7 +14,34 @@ router.set('views', path.join(__dirname,'views'));
 router.use(express.urlencoded()); //middleware
 router.use(express.static('assets')); //static
 
+// data storage
 
+let walletBalance = 0;
+
+var trades = [
+    {
+        name: 'btc',
+        leverage: 6,
+        entryPrice: 293, 
+        margin: 986
+    }
+]
+
+// post routers
+router.post('/add-money', function(req,res) {
+    walletBalance += parseInt(req.body.money);
+    //console.log(walletBalance);
+    return res.redirect('back');
+})
+
+router.post('/buy-btc', function(req,res) {
+    walletBalance -= parseInt(req.body.amount);
+
+    return res.redirect('/user');
+})
+
+
+// get routers
 router.get('/', function(req, res) {
     res.render('home');
 })
@@ -24,11 +51,15 @@ router.get('/register', function(req, res) {
 })
 
 router.get('/user', function(req,res) {
-    res.render('user');
+    res.render('user', {
+        walletbalance: walletBalance
+    });
 })
 
 router.get('/trade', function(req,res) {
-    res.render('trade');
+    res.render('trade', {
+        walletbalance: walletBalance
+    });
 })
 
 router.listen(port, function(err) {
